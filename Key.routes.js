5ece33add1604ebf62a9a888e30dd4b5373d11e5
gustavoflex22 <90397681+gustavoflex22@users.noreply.github.com>
2024-06-07 13:11:10 -0300

    Delete Key.routes.js

--- Key.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const keyController = require('../controller/KeyController');
-const keyRoutes = express.Router();
-
-keyRoutes.post('/create', keyController.create.bind(keyController));
-keyRoutes.delete('/delete/:id', keyController.delete.bind(keyController));
-
-module.exports = keyRoutes;
