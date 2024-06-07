7289f73a8c4c1e404d86ea8c6d84d269f249eb61
gustavoflex22 <90397681+gustavoflex22@users.noreply.github.com>
2024-06-07 12:42:42 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const keyRoutes = require('./Key.routes');
-const messageRoutes = require('./Message.routes');
-const routes = express.Router();
-
-routes.use('/key', keyRoutes);
-routes.use('/message', messageRoutes);
-
-module.exports = routes;
